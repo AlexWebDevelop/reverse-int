0a3f0f7c0c4e8ae3241dffca03a1acc7a06ab993
AlexWebDevelop <alexanderwolf417@gmail.com>
2021-03-05 21:37:52 +0300

    feat: end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = String(n).split("").reverse();
+    if (arr.includes("-")) {
+        arr.length = arr.length - 1;
+    }
+    return Number(arr.join(""));
+};
